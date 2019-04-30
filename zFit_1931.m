e20ef0b4c007b1582579a54deb58ac6c34583e51
Yahyaoui <50120459+ya7yawii@users.noreply.github.com>
2019-05-01 01:21:21 +0200

    Update zFit_1931.m

--- zFit_1931.m
+++ zFit_1931.m
@@ -1,5 +1,5 @@
 function int = zFit_1931(wave)
-t1 = 0.0845.*(1-heaviside(wave-437.0))+0.0278.*heaviside(wave-437.0);
-t2 = 0.0385.*(1-heaviside(wave-459.0))+0.0725.*heaviside(wave-459.0);
+t1 = 0.0845.*(1-stepfunction(wave-437.0))+0.0278.*stepfunction(wave-437.0);
+t2 = 0.0385.*(1-stepfunction(wave-459.0))+0.0725.*stepfunction(wave-459.0);
 int = 1.217.*exp(-0.5.*((wave-437.0).*t1).^2)+0.681.*exp(-0.5.*((wave-459.0).*t2).^2);
 end
